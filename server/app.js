765199c3a00945b6b5f782b53cf1afb63b562815
TinySymphony <zjutiny@gmail.com>
2016-12-16 15:32:00 +0800

    add server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,20 @@
+/* eslint-disable */
 var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var app = express()
 
-app.use()
+app.use(express.static(path.resolve(__dirname, '../dist/')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cookieParser())
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(server.address())
+})
